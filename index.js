e3c62c99bd179ed7b89b6575a2933bba447995eb
igk190 <graciakleijnen@gmail.com>
2025-10-26 18:49:32 +0000

    Add views folder, create pug files, make nav bar.

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index'); // renders index.pug
+});
+
+app.listen(3000, () => {
+  console.log('Server läuft auf http://localhost:3000');
+});
