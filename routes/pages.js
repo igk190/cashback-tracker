51fc1299315a8dd214c24545c357c58741096700
igk190 <graciakleijnen@gmail.com>
2025-10-28 00:11:26 +0000

    Error in retrieving DB data, passing it with router GET, and using loop to show all elements in pug cards. Destructuring not working, query returns first item from DB only.

--- routes/pages.js
+++ routes/pages.js
@@ -1,17 +1,48 @@
 import express from 'express';
 import { addCashbackOffer } from '../models/cashback.js';
+import pool from '../config/database.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index'); 
+router.get('/', async (req, res) => {
+    const conn = await pool.getConnection();
+    const offers = await conn.query('SELECT * FROM cashback_offer');
+    console.log("OFFERS", offers);
+
+    conn.release();
+      res.render('index'); 
+
 });
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard'); 
+router.get('/dashboard', async (req, res) => {
+  try {
+    const conn = await pool.getConnection();
+    const offers = await conn.query('SELECT * FROM cashback_offer');
+    console.log("OFFERS", offers);
+    conn.release();
+
+    // const offers = [
+    //   {
+    //     id: 1,
+    //     product_name: 'Test Pokemon',
+    //     photo_url: 'https://via.placeholder.com/150',
+    //     status: 'Available',
+    //     cashback_amount: '3.49',
+    //     store: 'Kaufland',
+    //     start_date: '2025-10-27',
+    //     end_date: '2025-10-31',
+    //     conditions: null,
+    //     cashback_url: 'https://example.com',
+    //     purchased_on: null
+    //   }
+    // ];
+    res.render('dashboard', { offers, success_msg: req.flash('success_msg') });
+  } catch (err) {
+    console.error('Error:', err);
+    req.flash('error_msg', 'Error loading offers.');
+    res.render('dashboard', { offers: [], success_msg: req.flash('success_msg') });
+  }
 });
-
-
 
 router.post('/cashback', async (req, res) => {
   try {
