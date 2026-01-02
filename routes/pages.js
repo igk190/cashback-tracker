fd2a982c5bb91e8389264ed4a15b98b700e84428
igk190 <graciakleijnen@gmail.com>
2026-01-02 16:09:28 +0100

    Fix get data for Cashback pending card.

--- routes/pages.js
+++ routes/pages.js
@@ -9,27 +9,51 @@ router.get('/', async (req, res) => {
 
 });
 
+// -------- GET CARDS DATA FOR DASH --------
+
+function getCompletedCardData(allCashbackOffers) {
+  let total_cashback_sum = 0;
+  let total_offers_completed = 0;
+
+  for (let index in allCashbackOffers){
+    if (allCashbackOffers[index].status === "Completed" ) {
+      total_cashback_sum += parseFloat(allCashbackOffers[index].cashback_amount);
+      total_offers_completed += 1;
+    }
+  }
+  return [total_cashback_sum, total_offers_completed];
+}
+
+function getCashbackPendingCardData(allCashbackOffers) {
+  let sum_pending = 0;
+  let total_offers_pending = 0;
+
+  for (let index in allCashbackOffers) {
+    if (allCashbackOffers[index].status === 'Confirmed') {
+      sum_pending += parseFloat(allCashbackOffers[index].cashback_amount);
+      total_offers_pending += 1;
+    }
+  }
+  return [sum_pending, total_offers_pending];
+}
+
+// --------
+
 router.get('/dashboard', async (req, res) => {
   try {
     const conn = await pool.getConnection();
     const allCashbackOffers = await conn.query('SELECT * FROM cashback_offer');
     console.log("OFFERS", allCashbackOffers[3]);
     conn.release();
-
-    let total_cashback_sum = 0;
-    let total_offers_completed = 0;
-
-    for (let index in allCashbackOffers){
-      if (allCashbackOffers[index].status === "Completed" ) {
-        total_cashback_sum += parseFloat(allCashbackOffers[index].cashback_amount);
- 
-        total_offers_completed += 1;
-      }
-    }
-
     
+    let [total_cashback_sum, total_offers_completed] = getCompletedCardData(allCashbackOffers); // 'Cashback received' card
+    let [sum_pending, total_offers_pending] = getCashbackPendingCardData(allCashbackOffers); // 'Cashback pending' card
 
-    res.render('dashboard', { allCashbackOffers, total_cashback_sum, total_offers_completed, success_msg: req.flash('success_msg') });
+    res.render('dashboard', { 
+      allCashbackOffers, 
+      total_cashback_sum, total_offers_completed,
+      sum_pending, total_offers_pending,
+      success_msg: req.flash('success_msg') });
   } catch (err) {
     console.error('Error:', err);
     req.flash('error_msg', 'Error loading offers.');
@@ -51,4 +75,4 @@ router.post('/cashback', async (req, res) => {
 export default router;
 
 
-// console.log(typeof allCashbackOffers[index].cashback_amount ); // string! Returned as string, parseFloat
+// console.log(typeof allCashbackOffers[index].cashback_amount ); // string! Returned as string, use parseFloat
